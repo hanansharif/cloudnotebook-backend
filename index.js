65465e8772c349419ffc7da961c5ebb04a751745
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2023-11-28 09:30:00 +0500

    initial setup

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ connetToMongo();
 const app = express()
 const port = 3000
 
+
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
